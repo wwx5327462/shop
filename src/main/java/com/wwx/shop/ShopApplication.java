2e3a29db26a085561b4ec8726a8db2ac54cabbb6
wwx5327462 <845140344@qq.com>
2020-12-19 20:54:22 +0800

    更改

--- src/main/java/com/wwx/shop/ShopApplication.java
+++ src/main/java/com/wwx/shop/ShopApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShopApplication {
 
     public static void main(String[] args) {
-        System.out.println("hehe");
         SpringApplication.run(ShopApplication.class, args);
     }
 
