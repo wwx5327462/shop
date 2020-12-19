e56a3ad21c79189b0cdc56f8cfd7627466b70f3d
wwx5327462 <845140344@qq.com>
2020-12-19 20:49:10 +0800

    这是修改后的记录

---
+++ src/main/java/com/wwx/shop/ShopApplication.java
@@ -0,0 +1,14 @@
+package com.wwx.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hehe");
+        SpringApplication.run(ShopApplication.class, args);
+    }
+
+}
